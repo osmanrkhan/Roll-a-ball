4ea5789921b9158d1afd560367d0cf8841868f5a
osmanrkhan <osmanrkhan1@gmail.com>
2016-10-30 13:39:51 +0500

    Add files via upload
    All the assets for the game.

---
+++ Assets/Scripts/CRotater.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CRotater : MonoBehaviour {
+	void Update() {
+		transform.RotateAround(Vector3.zero, Vector3.up, 5000 * Time.deltaTime);
+	}
+}
+
+
+
+
+// using UnityEngine; using System.Collections;
+
+//public class CRotater : MonoBehaviour {
+	
+	// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+		//transform.Rotate (new Vector2 (10, 15) * Time.deltaTime);
+		
+	//}
+//}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public Text countText;
+	
+	private Rigidbody rb;
+	private int count;
+	public Text wintext;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		wintext.text = "";
+
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pickup"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+		void SetCountText ()
+	{
+		countText.text = "SCORE: " + count.ToString ();
+		if (count >= 8)
+		{
+			wintext.text = "You Win!!";
+		}
+	}
+		
+	}
